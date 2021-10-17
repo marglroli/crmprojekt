eec7be132285b4cdf20e392760c99ed1ed81f421
dominikkrizsn <dominik.krizsn@gmail.com>
2021-10-18 00:20:55 +0200

    top info bar+ little logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,32 @@
             font-family: "Lato", sans-serif;
         }
 
+        .pic{
+            margin-left: 25px;
+            margin-bottom: 10px;
+            border-radius: 10%;
+        }
+
+        .top-container {
+            display: flex;
+            position: relative;
+            gap: 500px;
+            background-color: grey;
+            justify-content: flex-end;
+            align-items: center;
+            height: 120px;
+            margin-top: -10px;
+            margin-right: -10px;
+        }
+
+        .top-bar-text{
+            font-weight: 800;
+            font-size: 3rem;
+            color: white;
+            padding-right: 30px;
+            margin-right: 30px;
+        }
+
         .sidenav {
             height: 100%;
             width: 160px;
@@ -17,7 +43,7 @@
             left: 0;
             background-color: #111;
             overflow-x: hidden;
-            padding-top: 20px;
+            padding-top: 10px;
         }
 
         .sidenav a {
@@ -34,6 +60,7 @@
 
         .main {
             margin-left: 160px;
+            margin-top: 10px;
             /* Same as the width of the sidenav */
             font-size: 28px;
             /* Increased text to enable scrolling */
@@ -42,31 +69,35 @@
 
         @media screen and (max-height: 450px) {
             .sidenav {
-                padding-top: 15px;
+                padding-top: 100px;
             }
 
             .sidenav a {
                 font-size: 18px;
             }
         }
-
     </style>
 </head>
-
 <body>
-        <p>kisbuzi</p>
+    <div class="top-container">
+        <div>
+            <span class="top-bar-text">CRM SYSTEM</span>
+        </div>
+        <div>
+            <div class="top-bar-text">username</div>
+        </div>
+    </div>
     <div class="sidenav">
+        <div>
+            <img class="pic" height="100" src="https://avatarfiles.alphacoders.com/105/105223.jpg">
+        </div>
         <a href="/users">Users</a>
         <a href="#services">Services</a>
         <a href="#clients">Clients</a>
         <a href="#contact">Contact</a>
     </div>
-
     <div class="main">
-     @yield('main_content')
-     
+        @yield('main_content')
     </div>
-
 </body>
-
 </html>
